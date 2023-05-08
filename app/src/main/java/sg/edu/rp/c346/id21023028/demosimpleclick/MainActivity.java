a8c36a9f6d454ca9280d3cd7f989fb7b380cab83
Nigel-Tan <nigel.tan@hotmail.sg>
2023-05-08 11:01:15 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21023028/demosimpleclick/MainActivity.java
@@ -0,0 +1,34 @@
+package sg.edu.rp.c346.id21023028.demosimpleclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the action
+
+            }
+
+        }
+}
